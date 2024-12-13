d320d571cb0e391392314a62742428e9a95fc6e1
Nikki-kicki <nsibanov456@gmail.com>
2024-12-13 17:22:24 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,8 @@
+# переменные
+
+username = print('Пользователь',)
+title = print('Заголовок заметки')
+content = print('Описание заметки')
+status = print('Статус заметки')
+created_date = print('Дата создания заметки')
+issue_date = print('Дата истечения заметки')
