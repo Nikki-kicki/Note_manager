48adebef439d984c841cca25112c8954eda87dcd
Nikki-kicki <nsibanov456@gmail.com>
2024-12-15 09:18:42 +0300

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -1,9 +1,19 @@
+
+
 print('Добавтье до 3-ех ключевых слов')
 
+
 key_title1 = input()
-key_title2 = input()
-key_title3 = input()
+if key_title1:
+    print('Добавим еще?')
+    key_title2 = input()
+    if key_title2:
+        print('И еще одно')
+        key_title3 = input()
+        if key_title3:
+            print('Добавлено максимальное количество ключевых слов')
 
 key_list = [key_title1,key_title2,key_title3]
 
+print('Это ваши ключевые слова:')
 print(key_list)
