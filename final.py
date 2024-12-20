f085ea1798ba18c967f5c4e356b6c3033cbe0a52
Nikki-kicki <nsibanov456@gmail.com>
2024-12-20 12:49:43 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,14 +1,14 @@
-import greetings
-from add_list import key_list
 from greetings import username, title, content, status, created_date, issue_date
+from add_list import key_list
 
-note_info = [
-    username,
-    title,
-    content,
-    status,
-    created_date,
-    issue_date,
-    key_list
+# список данных введенных пользователем
+note_info = {
+    'username',
+    'title',
+    'content',
+    'status',
+    'created_date',
+    'issue_date',
+    'key_list'
 
-]
+}
